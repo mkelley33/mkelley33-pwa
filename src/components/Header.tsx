46738c9be56d8550c4202a8d9484cc96e2b99e86
Michaux Kelley <michauxkelley@gmail.com>
2023-09-23 09:48:04 -0400

    Update light and dark scheme CSS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <header className="flex flex-row justify-start fixed pl-10 w-full h-[100px] items-center gap-8 border-b-white border-b-4">
-      <Link href="/" className="text-4xl text-slate-50">
+      <Link href="/" className="text-4xl">
         @mkelley33:
       </Link>
       <Link href="/blog">Blog</Link>
