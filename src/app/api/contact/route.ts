6d48070023b20c848932f505c5252f667abbf7e4
Michaux Kelley <michauxkelley@gmail.com>
2023-10-11 09:21:56 -0400

    Fix contact form (#2)
    Shows actual error message in toast (temporary)

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -42,10 +42,9 @@ export async function POST(req: Request) {
     const json = await req.json();
     const response = await sendContactEmail(json);
     return NextResponse.json(response);
-  } catch {
-    // TODO: log sending email error
+  } catch (e: any) {
     return NextResponse.json({
-      error: 'Something went wrong. Please try again.',
+      error: e.message,
     });
   }
 }
