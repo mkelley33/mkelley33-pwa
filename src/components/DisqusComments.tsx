a014c63946d6481bd9a9edf5f3991fdef1130f41
Michaux Kelley <michauxkelley@gmail.com>
2023-10-12 09:23:43 -0400

    Add disqus comments to blog

---
+++ src/components/DisqusComments.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { DiscussionEmbed } from 'disqus-react';
+
+interface IPost {
+  post: {
+    title: string;
+  };
+  slug: string;
+}
+
+const DisqusComments = ({ post, slug }: IPost) => {
+  const disqusShortname = 'mkelley33';
+  const disqusConfig = {
+    url: `${process.env.NODE_ENV === 'production' ? 'https' : 'http'}://${
+      process.env.VERCEL_URL
+    }/blog/${slug}`,
+    identifier: slug,
+    title: post.title,
+  };
+
+  return (
+    <div>
+      <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+    </div>
+  );
+};
+
+export default DisqusComments;
