9e5badbca7a59dd8d930a929201fd196a02935a7
Michaux Kelley <michauxkelley@gmail.com>
2023-10-12 10:27:04 -0400

    Position footer at bottom of page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const copyrightYear = new Date().getFullYear();
 
   return (
-    <footer className="border-t-2 border-t-black pt-5 text-center">
+    <footer className="w-full border-t-2 border-t-black pt-5 text-center absolute bottom-0">
       <p>
         Copyright &copy; {copyrightYear} Michaux Kelley. All rights reserved.
       </p>
