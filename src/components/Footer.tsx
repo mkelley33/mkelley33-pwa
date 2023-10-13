84d12b81d00cf1102cbe85b3b3106cf0478204c2
Michaux Kelley <michauxkelley@gmail.com>
2023-10-13 07:50:42 -0400

    Update footer and main styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const copyrightYear = new Date().getFullYear();
 
   return (
-    <footer className="w-full border-t-2 border-t-black pt-5 text-center absolute bottom-0">
+    <footer className="bg-black w-full border-t-2 border-t-slate-50 pt-5 text-center fixed bottom-0 opacity-90">
       <p>
         Copyright &copy; {copyrightYear} Michaux Kelley. All rights reserved.
       </p>
