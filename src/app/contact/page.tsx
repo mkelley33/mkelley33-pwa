98aad3c30c93c6d90e1a55d324e58f1624394941
Michaux Kelley <michauxkelley@gmail.com>
2024-02-05 19:08:00 -0500

    Add some page titles (#15)

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,33 +1,10 @@
-'use client';
+import ContactFormContainer from './ConcactFormContainer';
+import { Metadata } from 'next';
 
-import ContactForm from './ContactForm';
-import HCaptcha from '@hcaptcha/react-hcaptcha';
-import { sendEmail } from '@/lib/send-email';
-import useHCaptcha from '@/hooks/useHCaptcha';
-import { verifyHCaptcha } from '@/lib/verify-hcaptcha';
-
-const ContactFormContainer = () => {
-  const { token, setToken, hCaptchaError, setHCaptchaError, captchaRef } = useHCaptcha();
-
-  return (
-    <ContactForm
-      sendEmail={sendEmail}
-      verifyHCaptcha={verifyHCaptcha}
-      token={token}
-      captchaRef={captchaRef}
-      setHCaptchaError={setHCaptchaError}
-      hCaptchaError={hCaptchaError}
-    >
-      <HCaptcha
-        sitekey={process.env.NEXT_PUBLIC_H_CAPTCHA_SITE_KEY!}
-        onVerify={setToken}
-        onError={() => setToken('')}
-        onExpire={() => setToken('')}
-        ref={captchaRef}
-        theme="dark"
-      />
-    </ContactForm>
-  );
+export const metadata: Metadata = {
+  title: 'Contact',
 };
 
-export default ContactFormContainer;
+export default function ContactPage() {
+  return <ContactFormContainer />;
+}
