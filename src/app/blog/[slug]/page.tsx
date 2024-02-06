19af272e237c5fdb0e383df2812fb369c04a2ae5
Michaux Kelley <michauxkelley@gmail.com>
2024-02-06 17:32:37 -0500

    Add dynamic titles to blog posts

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,18 +1,29 @@
-import { useMDXComponent } from 'next-contentlayer/hooks';
-import type { MDXComponents } from 'mdx/types';
-import { allPosts } from 'contentlayer/generated';
-import { format, parseISO } from 'date-fns';
-import { notFound } from 'next/navigation';
-import Link from 'next/link';
 import './prism.css';
+
+import { format, parseISO } from 'date-fns';
+
 import DisqusComments from '@/components/DisqusComments';
+import Link from 'next/link';
+import type { MDXComponents } from 'mdx/types';
+import { Metadata } from 'next';
+import { allPosts } from 'contentlayer/generated';
+import { notFound } from 'next/navigation';
+import { useMDXComponent } from 'next-contentlayer/hooks';
 
 interface IBlogPostProps {
   params: { slug: string };
 }
 
-export const generateStaticParams = async () =>
-  allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
+export async function generateMetadata({ params: { slug } }: IBlogPostProps): Promise<Metadata> {
+  const post = allPosts.find((post) => post._raw.flattenedPath === slug);
+
+  return {
+    title: `${post?.title} - mkelley33`,
+    description: post?.description,
+  };
+}
+
+export const generateStaticParams = async () => allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
 
 const BlogPost = ({ params: { slug } }: IBlogPostProps) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === slug);
@@ -30,10 +41,7 @@ const BlogPost = ({ params: { slug } }: IBlogPostProps) => {
     <>
       <article>
         <h1>{post.title}</h1>
-        <time
-          dateTime={post.date}
-          className="block text-xs uppercase font-bold mb-3"
-        >
+        <time dateTime={post.date} className="block text-xs uppercase font-bold mb-3">
           {format(parseISO(post.date), 'MMM dd, yyyy')}
         </time>
         <MDXContent components={mdxComponents} />
